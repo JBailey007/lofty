0cba1d7adef5c77c81ae266ca8eefdf6fb40d7aa
Nick Burr <nicholasstclairburr@gmail.com>
2023-09-25 22:22:42 -0600

    got the server to the point where it can actually start. verynice!

--- models/index.js
+++ models/index.js
@@ -1,13 +1,16 @@
 const User = require('./User');
-const Goal = require('./Goal');
+const Task = require('./Task');
+const Lofty = require('./Lofty');
+const Attainable = require('./Attainable');
 
-User.hasMany(Goal, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
 
-Goal.belongsTo(User, {
-  foreignKey: 'user_id'
-});
+// User.hasMany(Goal, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// });
 
-module.exports = { User, Goal };
+// Goal.belongsTo(User, {
+//   foreignKey: 'user_id'
+// });
+
+module.exports = { User, Task, Lofty, Attainable };
