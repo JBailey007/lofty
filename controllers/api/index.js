3ee5b35c99bcc66d40f09478f835c9a6f50b18ef
Nick Burr <nicholasstclairburr@gmail.com>
2023-09-26 21:55:29 -0600

    some routes have been updated a bit

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,14 +1,14 @@
 const attainable = require('./attainable-routes')
 const lofty = require('./lofty-routes')
 const task = require('./task-routes')
-// const user = require('./user-routes')
+const user = require('./user-routes')
 const router = require('express').Router()
 
-//user is turned off rn, uncomment 4 and 12 to turn user on 
+//user is turned on rn, uncomment 4 and 12 to turn user off
 
 router.use('/attainable', attainable)
 router.use('/task', task)
 router.use('/lofty', lofty)
-// router.use('/user', user)
+router.use('/user', user)
 
 module.exports = router
