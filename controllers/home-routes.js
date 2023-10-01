1770fd01aa6e1b46f24eabd52dff22f51f8db3a0
Jalen <jwbailey13@gmail.com>
2023-10-01 15:38:10 -0600

    updated the home-routes so that we are now gathering user data and not goal data

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,31 +2,60 @@ const router = require('express').Router();
 const { Task, Attainable, Lofty, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/api/', async (req, res) => {
+// router.get('/api/', async (req, res) => {
+//   try {
+//     // Get all projects and JOIN with user data
+//     const goalData = await Lofty.findAll({
+//       include: [
+//         {
+//           model: User,
+//           attributes: ['name'],
+//         },
+//       ],
+//     });
+
+//     // Serialize data so the template can read it
+//     const goals = goalData.map((goal) => goal.get({ plain: true }));
+
+//     // Pass serialized data and session flag into template
+//     res.render('login', { 
+//       goals,
+//       // logged_in: req.session.logged_in ------------------NEED TO TURN ON
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+//------------------------------------------------------------------------------------------
+router.get('/', withAuth, async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
-    const goalData = await Lofty.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Lofty }],
     });
 
-    // Serialize data so the template can read it
-    const goals = goalData.map((goal) => goal.get({ plain: true }));
+    const user = userData.get({ plain: true });
 
-    // Pass serialized data and session flag into template
-    res.render('login', { 
-      goals,
-      // logged_in: req.session.logged_in ------------------NEED TO TURN ON
+    res.render('login', {
+      ...user,
+      logged_in: true
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/lofty');
+    return;
+  }
+
+  res.render('login');
+});
+// -------------------------------------------------------------------------------------------
 router.get('/attainable', async (req, res) => {
   try {
     // Get all projects and JOIN with user data
