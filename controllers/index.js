2f13d875928caef449d1ad22d49928349e9c8910
uraniumrainbow <uraniumrainbow@gmail.com>
2023-10-01 13:20:13 -0600

    test test

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
-const attainableRoutes = require('./attainable-routes');
-const taskRoutes = require('.//task-routes');
+
 
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
-router.use('/attainable', attainableRoutes);
-router.use('/task', taskRoutes);
-
 
 module.exports = router;
